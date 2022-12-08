2160b111eba06f5916ecc38023dddb102e093120
Daniel Ching <danielching1@gmail.com>
2022-12-08 18:11:01 +0800

    A bit more work on the Enemy AI infrastructure

--- Assets/Scenes/Arena/Scripts/Enemy/Enemy.cs
+++ Assets/Scenes/Arena/Scripts/Enemy/Enemy.cs
@@ -6,10 +6,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, IEntity {
     [SerializeField] private string id; // Used by World_EnemyLibrary
     [SerializeField] private string enemyName;
-    
-    private World_Grid gridHandler;
-    private World_Turns turnsHandler;
-    private IEnemyAI ai;
+    [SerializeField] private IEnemyAI ai; // The AI this enemy will be using
 
     // Properties
     private int _health, _healthMax;
@@ -25,19 +22,18 @@ public class Enemy : MonoBehaviour, IEntity {
     // Getters
     public string GetID() { return id; }
     public string GetEnemyName() { return enemyName; }
-    public World_Grid GridHandler() { return gridHandler; }
-    public World_Turns TurnsHandler() { return turnsHandler; }
 
-    public void Setup(World_Grid _gridHandler, World_Turns _turnsHandler) {
-        gridHandler = _gridHandler;
-        turnsHandler = _turnsHandler;
-
-        ai = this.GetComponent<IEnemyAI>();
+    // A bit jank but the simplest way to dump only World_Grid into enemy AI
+    public void Setup(World_Grid _gridHandler) {
+        ai.Setup(_gridHandler);
     }
 
     public List<Task> ReturnCurrentTurn() { return ai.ReturnCurrentTurn(); }
 
     void Awake() {
+        // Sanity checks
+        if (ai == null) ai = this.GetComponent<IEnemyAI>();
+
         _healthMax = _health = 500; // Setting up enemy's health properties
     }
 }
--- Assets/Scenes/Arena/Scripts/Enemy/IEnemyAI.cs
+++ Assets/Scenes/Arena/Scripts/Enemy/IEnemyAI.cs
@@ -1,8 +1,11 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
+// using UnityEngine;
 
 //! This is the base class for enemy AI
 //! Do not modify this script unless you're sure you want to affect ALL enemy AIs
+//! A List<Task> is equivalent to a World_Turn as in a list of tasks is equal to 1 turn
 interface IEnemyAI {
+    void Setup(World_Grid _gridHandler);
     List<Task> ReturnCurrentTurn();
 }
