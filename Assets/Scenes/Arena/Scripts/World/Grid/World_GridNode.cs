3c221fc5cfddcb4e4f3d0e6c7ad36b1281f5297b
Frog966 <danielching1@gmail.com>
2022-12-14 16:16:56 +0800

    Added animations to grid nodes

--- Assets/Scenes/Arena/Scripts/World/Grid/World_GridNode.cs
+++ Assets/Scenes/Arena/Scripts/World/Grid/World_GridNode.cs
@@ -1,15 +1,24 @@
-// using System.Collections;
-// using System.Collections.Generic;
 using UnityEngine;
 
 public class World_GridNode : MonoBehaviour {
     [SerializeField] private bool isPlayerControlled = true;
     
+    [SerializeField] private GameObject telegraph;
+    [SerializeField] private Animator animator;
     [SerializeField] private UnityEngine.UI.Image top, front;
+    
+    [Header("Node Colors")]
     [SerializeField] private Color color_Player, color_Enemy;
 
     // Getters
     public bool IsPlayerControlled() { return isPlayerControlled; }
+
+    // Animations
+    // No waiting here as multiple nodes might be performing animations at the same time
+    public void PlayAnim_Flash() { animator.SetBool("isFlash", true); }
+    public void StopAnim_Flash() { animator.SetBool("isFlash", false); }
+    public void PlayAnim_Flicker() { animator.SetBool("isFlicker", true); }
+    public void StopAnim_Flicker() { animator.SetBool("isFlicker", false); }
 
     // Set isPlayerControlled + update image color accordingly
     public void SetIsPlayerControlled(bool b) {
@@ -17,5 +26,11 @@ public class World_GridNode : MonoBehaviour {
 
         isPlayerControlled = b;
         top.color = front.color = isPlayerControlled ? color_Player : color_Enemy; // Set image according to isPlayerControlled bool
+    }
+
+    void Awake() {
+        telegraph.SetActive(false);
+
+        if (animator == null) this.GetComponent<Animator>();
     }
 }
