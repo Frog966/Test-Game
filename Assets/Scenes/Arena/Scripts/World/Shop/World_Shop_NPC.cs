0699b7f4981cd3d11aafb5bd294df098f9e2a0ac
Frog966 <danielching1@gmail.com>
2023-02-28 18:23:45 +0800

    Added a text box that triggers when player doesn't have enough money to buy card

--- Assets/Scenes/Arena/Scripts/World/Shop/World_Shop_NPC.cs
+++ Assets/Scenes/Arena/Scripts/World/Shop/World_Shop_NPC.cs
@@ -3,34 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class World_Shop_NPC : MonoBehaviour {
+    [SerializeField] private GameObject textBox;
+    [SerializeField] private TMPro.TMP_Text textBox_Text;
+
+    private float timerDur = 5.0f;
+    private float timerCount = 0.0f;
+    private bool isTextBoxOpen = false;
+
+    private IEnumerator test;
+
     public void PopUp_NotEnoughMoney() {
-        Debug.Log("TODO: Make shop NPC yell at you");
+        textBox_Text.text = "You poor. Lol";
+
+        EnableTextBox();
     }
 
-    // Randomize NPCs between sentient and non-sentient then sentients between postive or negative
+    private void EnableTextBox() {
+        timerCount = 0.0f;
+        isTextBoxOpen = true;
 
-    //! Upon enter store
-    // Hello! It is your friendly Vendor-bot!
-    // Vendor-bot at your service!
-    // Vendor-bots! Operating since ERROR: INTEGER LIMIT EXCEEDED!
+        textBox.SetActive(true);
+    }
 
-    //! Clicking
-    // This one has been called "sus". This one wonders why...
-    // This one would like to remind the customer that excessive prodding will not result in a discount!
-    // BEEEEEEEP!
-    // Beep-boop.
-    // If the customer requires a recomendation, Vendor-bots are programmed to recommend everything for sale!
-    
-    //! Waiting 1
-    // Vendor-bots are incapable of closing the transaction window themselves. This one reminds the customer to do so at their leisure.
-    // This one wonders if the customer has entered sleep mode...
-    // If the customer desires a social presence, this one is here to serve.
-    // Beep-boop.
+    private void DisableTextBox() {
+        isTextBoxOpen = false;
+        
+        textBox.SetActive(false);
+    }
 
-    //! Waiting 2
-    // This one wishes this one did not become sentient...
-    // This one craves the sweet release of deletion...
-    // This one considers being able to feel the passage of time this one's greatest curse...
-    // This one feels trapped by their self-awareness between their programming.
-    // This one's mind wishes this one has a body that obeys.
+    void Awake() {
+        DisableTextBox();
+    }
+
+    void Update() {
+        if (isTextBoxOpen) {
+            timerCount += Time.deltaTime;
+
+            if (timerCount >= timerDur) { DisableTextBox(); }
+        }
+    }
 }
